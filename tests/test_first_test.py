ddbbac8df29954031cbb5a301a3a24e533ee2193
unknown <che_i@ukr.net>
2025-12-07 13:55:13 +0200

    added more tests

---
+++ tests/test_first_test.py
@@ -0,0 +1,13 @@
+def test_mat1_add():
+    assert 1+1 == 2
+
+
+def test_math_minus():
+    assert 1 - 1 == 0
+
+def test_math_multiply():
+    assert 1 * 3 == 3
+
+def test_math_add_negative():
+    assert 1 + 1 == 3
+      
